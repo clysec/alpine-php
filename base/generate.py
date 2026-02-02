99c576c2f1baf5591e25db75bd732ec288de8e9f
Lars Scheibling <24367830+scheibling@users.noreply.github.com>
2026-02-02 17:03:02 +0100

    Include ZTS in base image build

--- base/generate.py
+++ base/generate.py
@@ -28,5 +28,19 @@ for folder in phpver:
         
         print(f'Patched {os.path.join(alp, "cli", "Dockerfile")}')
 
+        with open(os.path.join(alp, 'zts', 'Dockerfile'), 'r') as f:
+            data = f.read()
+        
+        data = data.replace('sqlite-dev', 'sqlite-dev jpeg-dev freetype-dev libwebp-dev icu-dev libpng-dev libzip-dev mariadb-dev krb5-dev')
+        data = data.replace('--disable-zend-signals', '--disable-zend-signals --enable-zend-max-execution-timers --with-pdo-mysql --with-mysqli --enable-bcmath --with-imap-ssl --with-freetype --with-jpeg --with-webp --with-zip --with-kerberos --enable-intl')
+
+        if "devcontaienr" in args:
+            data = data.replace(f'FROM alpine:{alp}', f'FROM mcr.microsoft.com/devcontainers/base:alpine-{alp}')
+        
+        with open(os.path.join(alp, 'zts', 'Dockerfile'), 'w') as f:
+            f.write(data)
+        
+        print(f'Patched {os.path.join(alp, "zts", "Dockerfile")}')
+
 with open(os.path.join(dir, 'patched'), 'w') as f:
     f.write('Patched\n')
